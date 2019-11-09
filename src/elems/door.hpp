ee1241cb6fdb5e4960b70b1352f6970dc977ea9b
Valentin Chaboche <valentinchb@gmail.com>
2019-11-09 14:39:56 +0100

    add bool isExit to door

--- src/elems/door.hpp
+++ src/elems/door.hpp
@@ -6,11 +6,13 @@
 class Door: public Elem{
     private:
         bool opened;
+        bool isExit;
     public:
-        Door(Game*, int, int);
+        Door(Game*, int, int, bool);
         bool isOpened();
         void open();
         bool blockable();
+        bool isDoorExit();
 };
 
 #endif
