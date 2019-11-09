f4a84385c3719eed96f06190ce974df01f924148
Valentin Chaboche <valentinchb@gmail.com>
2019-11-09 11:49:12 +0100

    board bound acces

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -9,15 +9,22 @@ void Game::add_board(Board* b){
 }
 
 void Game::print(){
+  if(Game::current_board>0 && Game::current_board<(int)Game::boards.size()){
     Game::boards.at(Game::current_board)->print();
+  }
 }
 
 void Game::nextTurn() {
+  if(Game::current_board>0 && Game::current_board<(int)Game::boards.size()){
     for (Elem* e : boards.at(current_board)->getElems()){
-        e->takeAction();
+      e->takeAction();
     }
+  }
 }
 
 Board* Game::getCurrentBoard(){
+  if(Game::current_board>0 && Game::current_board<(int)Game::boards.size()){
+    return nullptr;
+  }
   return Game::boards.at(Game::current_board);
 }
