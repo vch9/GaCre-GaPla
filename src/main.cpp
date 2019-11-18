20d1d92e7ef1719adbd6f7339f296e64ce74d6f5
Valentin Chaboche <valentinchb@gmail.com>
2019-11-18 11:22:44 +0100

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #include "game/game.hpp"
 #include "board/board.hpp"
 #include "files/generate_board.hpp"
+#include "actions/health.hpp"
 
 int main(){
     string path1 = "../files/example.board";
@@ -13,7 +14,7 @@ int main(){
     Board* b1 = GenerateBoard::createBoard(game, path1);
     Board* b2 = GenerateBoard::createBoard(game, path2);
 
-    Elem* player = new Player(game, 1);
+    Elem* player = new Player(game, 1, 100);
 
     game->addBoard(b1);
     game->addBoard(b2);
@@ -21,4 +22,6 @@ int main(){
     game->setPlayer(player);
 
     game->play();
+
+    delete(game);
 }
